3b61aeb21a048f11a0d8c725f643ecacc89b5888
dimaskhosyi <64787626+dimaskhosyi@users.noreply.github.com>
2020-05-04 20:39:03 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {
+	stickyNavbar();
+};
+
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function stickyNavbar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
